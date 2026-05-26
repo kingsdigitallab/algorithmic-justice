ea0082afe1b8a96c2b8dd1618c8a561c1337c88a
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-05-26 22:48:48 +0000

    feat(cache): add loadCache() method [opencode:big-pickle]

--- app/cached-inference-engine.mjs
+++ app/cached-inference-engine.mjs
@@ -6,13 +6,28 @@ export default class CachedInferenceEngine extends InferenceEngine {
     this.cache = {}
   }
 
+  async loadCache(url) {
+    let ret = {}
+    try {
+      const response = await fetch(url)
+      if (response.ok) {
+        ret = await response.json()
+      }
+    } catch (err) {
+      console.error('Error loading cache:', err)
+    }
+    this.cache = ret
+  }
+
   async sendPrompt(prompt) {
     let key = InferenceEngine.hash(`${this.model}-${prompt}`)
     if (key in this.cache) {
       return this.cache[key]
     }
     let ret = await super.sendPrompt(prompt)
-    this.cache[key] = ret
+    if (ret) {
+      this.cache[key] = ret
+    }
     return ret
   }
 }
